16d8246103d5edeaa2582d34d5b60221a91461d1
Nina Herrera <ninaeherrera@gmail.com>
2020-08-13 14:28:35 -0500

    models, associations and validations done, routing, controller and views for cocktails done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts 'Seeding ingredients...'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+ingredients_json = open(url).read
+ingredients = JSON.parse(ingredients_json)
+
+ingredients['drinks'].each do |e|
+  Ingredient.create(name: e['strIngredient1'])
+end
+
+puts 'Finished'
+
